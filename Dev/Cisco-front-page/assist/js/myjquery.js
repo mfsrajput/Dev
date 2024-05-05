c13ba20842e63145d67d9172e71ec2fbd0fcbd2f
farhan-rajput <mfs.khi@gmail.com>
2024-05-05 13:10:11 +0500

    adding my cisco page

---
+++ Dev/Cisco-front-page/assist/js/myjquery.js
@@ -0,0 +1,39 @@
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3,
+//     spaceBetween: 30,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+
+
+let swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 5,
+      slideShadows: true,
+  },
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+  },
+  navigation: {
+  nextEl: ".swiper-button-next",
+  prevEl: ".swiper-button-prev",
+},
+initialSlide: 3, 
+loop: true, // Enable looping
+loopAdditionalSlides: 1, 
+});
